c384c82e32f0aabd2e91fffe840513edd5739d30
Poonam Maurya <io3.poonam30@gmail.com>
2023-06-29 20:28:01 +0530

    add challenges

--- myproject/src/App.js
+++ myproject/src/App.js
@@ -4,12 +4,15 @@ import Home from './component/Home';
 import SubPart from './component/SubPart';
 import Count from './component/count';
 import Footer from './component/Footer';
+import Challenge from './component/Challenge';
+
 
 
 function App() {
   return (
     <div>
      <Count/>
+     <Challenge/>
      <Home />
      <SubPart />
      <Footer/>
