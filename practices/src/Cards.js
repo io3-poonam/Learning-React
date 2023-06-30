52492d224a21562703ddbe8dc648ea8689ac2ec3
Poonam Maurya <io3.poonam30@gmail.com>
2023-06-30 22:34:07 +0530

    cards modified

--- practices/src/Cards.js
+++ practices/src/Cards.js
@@ -1,12 +1,15 @@
 import React from "react";
+import Heading from "./Heading";
+import Image from "./Image";
+
 function Cards(props){
     return(
         <>
         <div className="cards">
             <div className="card">
-                <img src={props.imgSrc} alt="gng college" className="img_card" width={'600px'}/>
+                <Image imgSrc={props.imgSrc} />
             <div className="cards_details">
-                <h3 className="card_title" >{props.title}</h3>
+                <Heading title={props.title}/>
                 <p className="card_Address">{props.Address}</p>
                 <p className="card_number">{props.number}</p>
                 <a href={props.links} target="_blank">  <button>Click</button></a>
