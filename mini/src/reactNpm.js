2bf3cbe07b1bb8f35bb3e9c43e991bee4248a56d
Poonam Maurya <io3.poonam30@gmail.com>
2023-07-17 21:50:46 +0530

    practices

---
+++ mini/src/reactNpm.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Clock from 'react-live-clock';
+import { CountdownCircleTimer } from 'react-countdown-circle-timer'
+
+const ReactNpm=()=>{
+    return(<>
+    <Clock/>
+    <CountdownCircleTimer/></>)
+}
+export default ReactNpm
