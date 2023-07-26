be7d8e3cbfada9a6fd5547358823b6bb63250f5f
Poonam Maurya <io3.poonam30@gmail.com>
2023-07-26 18:31:12 +0530

    component

--- myproject/src/component3/createNote.js
+++ myproject/src/component3/createNote.js
@@ -1,7 +1,10 @@
 import React, { useState } from "react";
 import './style.css'
+import AddIcon from '@mui/icons-material/Add';
+
 import NoteBox from "./noteBox";
 const CreateNote=()=>{
+    const[expand,setExpand]=useState(false)
     const[note1,setNote1]=useState({
         title:"",content:""
     });
@@ -21,17 +24,32 @@ console.log('note', note1)
         console.log('note1',note1)
     }
     const onDelete=(id)=>{
-        setAddNote1((deleteITEMS)=>{
-            return deleteITEMS.filter(curr,ind)
-            return ind !== id;
+        setAddNote1((deleteItems)=>{
+            return (deleteItems.filter((currdata,indexValue)=>{
+                return indexValue !==id
+            }))
         })
     }
+     {/* hidden button and input section with the help of ternary operator*/}
+            {/* pass useState false check condition in ture */}
+
+    const expandIt=()=>{
+        setExpand(true)
+    }
+    const dBClick=()=>{
+        setExpand(false)
+
+    }
     return(<>
-    <div className=" main_note">
+    <div className=" main_note" onDoubleClick={dBClick}>
         <form className="form">
-            <input name="title" value={note1.title} onChange={inputEventNote} id="input" type="text" placeholder="Title" autoComplete="off"/>
-            <textarea name="content" value={note1.content } onChange={inputEventNote} className="textarea" rows='' column="" placeholder="Write a note.."></textarea>
-            <button className="plus-sign" onClick={(pd)=>HandleBtnClick1(pd)}> ➕</button>
+            {expand?
+            <input name="title" value={note1.title} onChange={inputEventNote} id="input" type="text" placeholder="Title" autoComplete="off"/>:null}
+            <textarea name="content" value={note1.content } onChange={inputEventNote} className="textarea" rows='' column="" placeholder="Write a note.." onClick={expandIt}></textarea>
+            {/* hidden button section with the help of ternary operator  */}
+            {expand?
+            <button className="MuiButton-root" onClick={(pd)=>HandleBtnClick1(pd)}><AddIcon className="plus-sign"/> </button>:null}
+            {/* pass useState false check condition in true */}
         </form>
     </div>
     {addNote1.map((valueItem,index)=>{
--- myproject/src/component3/noteBox.js
+++ myproject/src/component3/noteBox.js
@@ -2,17 +2,13 @@ import React from "react";
 // import DeleteIcon from '@mui/icons-material/Delete';
 import "./style.css"
 const NoteBox=(props)=>{
-    const onDeleteBtn=()=>{
-        props.deleteItem(props.id)
-    }
-    console.log('m',onDeleteBtn)
     // console.log('p[rops',props)
     return(<>
     <div className="note">
         <h1 className="">{props.title}</h1>
         <br/>
         <p>{props.content}</p>
-        <button className="btn" onClick={onDeleteBtn}>
+        <button className="btn" onClick={()=>{props.deleteItem(props.id)}}>
         {/* <DeleteIcon className="deleteIcon"/> */}
         ✖️</button>
     </div>
