f57b2814b6b977299bd6302e098587158b0718f3
Poonam Maurya <io3.poonam30@gmail.com>
2023-08-05 20:17:50 +0530

    mini

--- mini/src/App.js
+++ mini/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import Challenge from './Challenge';
 import Button from './ButtonClick';
-import Game from './Game';
+import  Xyz from './xyz';
 import Clock from './clock';
 import Events from './Events';
 import Forms from './forms';
@@ -12,21 +12,14 @@ import Bootstrap from './bootstrap'
 import './game.css';
 function App() {
   return (<>
+  <Xyz/>
   <Bootstrap/>
   <Shopping/>
     <Button/>
   <Forms/>
   <Events/>
     <Clock/>
-      <h1 className='Heading_Game'>🎰 Welcome to slot Game machine 🎰</h1>
-     <div className='slotGame'> 
      
-      <Game  x='😊'y="😂"z="😢"/>
-      <Game  x='🍎'y="🍎"z="🍎"/>
-      <Game  x='⚽'y="📚"z="📚"/>
-      <Game  x='❤️'y="❤️"z="❤️"/>
-      <Game  x='🚗'y="🚗"z="🚗"/>
-      </div>
       <Challenge/>
     </>
   );
