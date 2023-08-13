f9dd46ad471b0febc1f853173b8d8fe28b9e487c
Poonam Maurya <io3.poonam30@gmail.com>
2023-08-13 22:29:13 +0530

    material

---
+++ materialui/src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+// import ResponsiveDialog from "material";
+import ResponsiveDialog from './component/material';
+function App() {
+  return (
+<>
+<ResponsiveDialog/></>
+  );
+}
+
+export default App;
