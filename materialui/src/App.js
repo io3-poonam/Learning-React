4abfe338b1c519e4b9ae3b03bee1000ef25f4d38
Poonam Maurya <io3.poonam30@gmail.com>
2023-08-17 16:45:13 +0530

    dialog

--- materialui/src/App.js
+++ materialui/src/App.js
@@ -5,9 +5,11 @@ import ResponsiveDialog from './component/material';
 import AlertDialog from './component/test';
 import FormDialog from './component/test1';
 import Testing from './component/testingTodo';
+import CustomizedDialogs from './component/dialog';
 function App() {
   return (
 <>
+<CustomizedDialogs/>
 <Testing/>
 <ResponsiveDialog/>
 <AlertDialog/>
